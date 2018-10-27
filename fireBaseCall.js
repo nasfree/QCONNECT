3e351ea04529ff39a713a1c46f3f5d1419f3e46a
kjordan101 <khernandez61600@Gmail.com>
2018-10-27 19:17:18 -0400

    Added working authentication

---
+++ fireBaseCall.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAkwsP3t3xAolOKxnDOs95z1crtNqMjJJg",
+  authDomain: "qconnect-d0b51.firebaseapp.com",
+  databaseURL: "https://qconnect-d0b51.firebaseio.com",
+  projectId: "qconnect-d0b51",
+  storageBucket: "",
+  messagingSenderId: "553778053970"
+};
+firebase.initializeApp(config);
--- signin.js
+++ signin.js
@@ -8,11 +8,36 @@ var config = {
   messagingSenderId: "553778053970"
 };
 firebase.initializeApp(config);
-// let users = firebase.database().ref("users");
-//
-//  let username = document.querySelector('#username');
-//  let passWord = document.querySelector('#password');
- firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
-   let errorCode = error.code;
-   let errorMessage = error.message;
- })
+ var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>',
+  // Privacy policy url.
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+ui.start('#firebaseui-auth-container', uiConfig);
